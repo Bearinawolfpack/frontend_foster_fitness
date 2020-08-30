9200c1e15187fd5179bc59c2cdc85deb67c459ec
bearinawolfpack <bearinawolfpack@gmail.com>
2020-08-30 09:06:32 -0400

    Contact form emails the admin

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ NOTES:
 
 - Icons https://react-icons.github.io/
 
+- Google Calendar:
+  - How to Embed a Google Calendar into your website: https://support.google.com/calendar/answer/41207?hl=en
+
+  - Embedded Google Calendar: https://calendar.google.com/calendar/embedhelper?src=k79dsr7vr90v9ip6rbr0l1b7v0%40group.calendar.google.com&ctz=America%2FNew_York
+
+  - NPM Package for React IFrames: https://www.npmjs.com/package/react-iframe
+
--- src/components/forms/signup_form.js
+++ src/components/forms/signup_form.js
@@ -1,26 +1,99 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form'
-import Layout from '../layout'
-import Button from 'react-bootstrap/Button'
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Layout from "../layout";
+import Button from "react-bootstrap/Button";
+import emailjs from "emailjs-com";
 
-const Signup = () => (
-    <Layout>
-        <Form.Group>
-            <Form.Label>Name:</Form.Label>
-            <Form.Control type="text" placeholder="Enter Your Name" value=''/>
-            <br />
-            <Form.Label>Email:</Form.Label>
-            <Form.Control type="text" placeholder="Enter Your Email" value=''/>
-            <br />
-            <Form.Label>Phone:</Form.Label>
-            <Form.Control type="text" placeholder="Enter Your Phone Number" value=''/>
-            <br />
-            <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows="3" placeholder="Tell us what's on your mind..." value=''/>
-            <br />
-            <Button variant="dark" type="submit">Submit</Button>
-        </Form.Group>
-    </Layout>
-)
+class Signup extends React.Component {
+	state = {
+		name: "",
+		email: "",
+		phone: "",
+		message: "",
+	};
 
-export default Signup
+	changeHandler = (event) => {
+		// console.log(event.target.value)
+		// console.log(event.target.name)
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	testSubmit = (event) => {
+		event.preventDefault();
+		console.log("SUBMIT IS WORKING");
+	};
+
+	sendEmail = (event) => {
+		event.preventDefault();
+
+		emailjs
+			.sendForm(
+				"gmail",
+				"fosterfitness",
+				event.target,
+				"user_E5T6aWsRG4CXjTwS6gRyl"
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+		event.target.reset();
+	};
+
+	render() {
+		return (
+			<Layout>
+                <Form onSubmit={this.sendEmail}>
+                    <Form.Group role="form">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="name"
+                            placeholder="Enter Your Name"
+                            value={this.state.name}
+                            onChange={this.changeHandler}
+                        />
+                        <br />
+                        <Form.Label>Email:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="email"
+                            placeholder="Enter Your Email"
+                            value={this.state.email}
+                            onChange={this.changeHandler}
+                        />
+                        <br />
+                        <Form.Label>Phone:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="phone"
+                            placeholder="Enter Your Phone Number"
+                            value={this.state.phone}
+                            onChange={this.changeHandler}
+                        />
+                        <br />
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            name="message"
+                            rows="3"
+                            placeholder="Tell us what's on your mind..."
+                            value={this.state.message}
+                            onChange={this.changeHandler}
+                        />
+                        <br />
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form.Group>
+                </Form>
+			</Layout>
+		);
+	}
+}
+
+export default Signup;
