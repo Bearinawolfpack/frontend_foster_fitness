ec2f0c9b8c62ee562a39729c4a92b610244392e6
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-01 17:48:21 -0400

    Fixed workout_card to reflect backend table change from workout to wod.

--- src/components/cards/workout_card.js
+++ src/components/cards/workout_card.js
@@ -5,7 +5,7 @@ const WorkoutCard = (props) => (
 	<Layout>
         <h3>Today is {props.wod.date} and the Workout for today is:</h3>
 		<h1>"{props.wod.name}"</h1>
-		<h3>{props.wod.workout}</h3>
+		<h3>{props.wod.wod}</h3>
 		Summary: <h5>{props.wod.description}</h5>
 	</Layout>
 );
