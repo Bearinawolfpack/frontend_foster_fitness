78d568a6b27553aaf2eea62729cdd4529855e5a4
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-01 18:58:39 -0400

    Roughed in service form to admin dashboard

--- src/components/forms/services_edit.js
+++ src/components/forms/services_edit.js
@@ -4,12 +4,76 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 class ServicesEdit extends React.Component {
+    state = {
+        name: "",
+        description: "",
+		price: "",
+	};
+
+	changeHandler = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	submitHandler = (event) => {
+		event.preventDefault();
+
+		fetch("http://localhost:3000/services", {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+			},
+			body: JSON.stringify({
+                "name": this.state.name,
+                "description": this.state.description,
+				"price": this.state.price,
+			}),
+		})
+			.then(this.resetSignup)
+    };
+    
+    resetSignup = () => {
+		this.setState({
+			name: "",
+			description: "",
+			price: "",
+		});
+	};
+
 	render() {
 		return (
 			<Layout>
-				<Form>
+				<Form onSubmit={this.submitHandler}>
 					<Form.Group role="form">
 						<Form.Label>Services Edit Form:</Form.Label>
+						<Form.Control
+							type="text"
+							name="name"
+							placeholder="Name of the Service"
+							value={this.state.name}
+							onChange={this.changeHandler}
+						/>
+						<br />
+						<Form.Control
+							type="text"
+							name="description"
+							placeholder="What is this Service for?"
+							value={this.state.description}
+							onChange={this.changeHandler}
+						/>
+						<br />
+                        <Form.Control
+                            type="number"
+                            step="0.01"
+							name="price"
+							placeholder="Price of Service"
+							value={this.state.price}
+							onChange={this.changeHandler}
+						/>
+						<br />
+						<Button variant="primary" type="submit">
+							Submit
+						</Button>
 					</Form.Group>
 				</Form>
 			</Layout>
