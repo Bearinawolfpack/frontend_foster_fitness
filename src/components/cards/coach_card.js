914a60af464d6fe566f5524d798df8da2ee5badf
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-07 17:10:53 -0400

    adjusted coach_card by adding marginTop so they aren't flush against the navBar

--- src/components/cards/coach_card.js
+++ src/components/cards/coach_card.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, Col } from "react-bootstrap";
-import styled from "styled-components";
-
-const Styles = styled.div`
-	
-`
 
 
 const CoachCard = (props) => (
 	<Col>
 		<nav>
 			<Link to={`/coach/${props.coach.id}`}>
-				<Card bg="transparent" style={{ width: "18rem", border: "0px solid rgba(0,0,0,.125)"}}>
+				<Card bg="transparent" style={{ width: "18rem", border: "0px solid rgba(0,0,0,.125)", marginTop: "30px" }}>
 					<Card.Img
 						src={props.coach.picture}
 						alt="Foster Fitness Coach"
