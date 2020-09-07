c51758cd9896672106dfed34f1cfe46553596e01
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-07 17:51:53 -0400

    Added placeholder styles and cards to events page and event cards

--- src/components/cards/events_card.js
+++ src/components/cards/events_card.js
@@ -1,19 +1,24 @@
 import React from "react";
-import Layout from "../layout";
-import Button from 'react-bootstrap/Button'
+import { Card, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 const EventCard = (props) => (
-	<Layout>
-		<h3>{props.event.name}</h3>
-		<img
-			src={props.event.picture}
-			alt="Wodapalooza event"
-			width="800"
-			height="500"
-		/>
-		<p>{props.event.description}</p>
-        <Button variant="link"><a href={props.event.url}>More Information</a></Button>
-	</Layout>
+	<Col>
+		<Card style={{ width: "50 rem" }}>
+			<Card.Title>{props.event.name}</Card.Title>
+			<Card.Img
+				variant="top"
+				src={props.event.picture}
+				alt="Wodapalooza event"
+			/>
+			<Card.Text>{props.event.description}</Card.Text>
+			<Button variant="link">
+				<a href={props.event.url}>More Information</a>
+			</Button>
+		</Card>
+	</Col>
 );
+
+
 
 export default EventCard;
--- src/containers/schedule.js
+++ src/containers/schedule.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import Calendar from '../components/calendar'
-import Events from '../components/events'
+import React from "react";
+import Calendar from "../components/calendar";
+import Events from "../components/events";
+import { Row, Container } from "react-bootstrap";
+import styled from "styled-components";
+import ring_pano_bw2 from "../assets/ring_pano_bw2.jpeg";
+
+const Wrapper = styled.div`
+	background-image: url(${ring_pano_bw2});
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-attachment: fixed;
+	height: 100%;
+`;
 
 const Schedule = () => (
-    <>
-        <h4>Coaches Work = Staff Class / GPP = Normal Classes / Oly = Lifting Classes / Part = Partner Workouts</h4>
-        <Calendar/>
-        <Events/>
-        For the most recent new on our events, please check our Instagram for Facebook.
-    </>
-)
+	<Wrapper>
+		<Container>
+			<Row>
+				<Calendar />
+			</Row>
+			<Row xs={1} md={2} lg={3}>
+				<Events />
+			</Row>
+		</Container>
+	</Wrapper>
+);
 
-export default Schedule
+export default Schedule;
