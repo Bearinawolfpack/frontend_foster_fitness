fe5a3d5751f2f682abf7a76a80f554cc05aed13a
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-08 21:45:51 -0400

    Refactored workout_jumbotron.js style. Fixed an issue where style sheet was changing all bootstrap jumbotrons by changing default bootstrap names in the style sheets.

--- src/components/cards/workout_jumbotron.js
+++ src/components/cards/workout_jumbotron.js
@@ -1,47 +1,17 @@
 import React from "react";
+import "../css/cards.css";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
-import styled from "styled-components";
-import bw_mess from "../../assets/bw_mess.jpeg";
-
-const Styles = styled.div`
-	.jumbo {
-		background: url(${bw_mess}) no-repeat fixed bottom;
-		background-size: cover;
-		color: #ccc;
-		height: 30rem;
-		position: relative;
-		z-index: -2;
-		margin-bottom: 0px;
-	}
-
-	.overlay {
-		background-color: #000;
-		opacity: 0.6;
-		position: absolute;
-		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
-		z-index: -1;
-	}
-	h1 {
-		color: #fcba03;
-	}
-`;
-
 
 const WorkoutJumbotron = (props) => (
-	<Styles>
-		<Jumbo fluid className="jumbo">
-			<div className="overlay" ></div>
-			<Container id="center-jumbo-content">
-				<h3>Today is {props.wod.date} and the Workout for today is:</h3>
-				<h1>"{props.wod.name}"</h1>
-				<h3>{props.wod.wod}</h3>
-				Summary: <h5>{props.wod.description}</h5>
-			</Container>
-		</Jumbo>
-	</Styles>
+	<Jumbo fluid className="workout-jumbo">
+		<div className="overlay"></div>
+		<Container id="center-jumbo-content">
+			<h3>Today is {props.wod.date} and the Workout for today is:</h3>
+			<h1 className="gym-text">"{props.wod.name}"</h1>
+			<h3>{props.wod.wod}</h3>
+			Summary: <h5>{props.wod.description}</h5>
+		</Container>
+	</Jumbo>
 );
 
 export default WorkoutJumbotron;
--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./css/components.css"
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 
 const Jumbotron = () => (
-	<Jumbo fluid className="jumbo">
+	<Jumbo fluid className="home-jumbo">
 		<div className="overlay"></div>
 		<Container id="center-jumbo-content">
-			<h1>Foster Fitness</h1>
+			<h1 className="gym-text">Foster Fitness</h1>
 			<p>
 				This Place Shall Never Be With Those Cold And Timid Souls Who
 				Neither Know Victory Nor Defeat!
