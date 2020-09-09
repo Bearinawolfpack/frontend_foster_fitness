f5eae163fdd946ae4f3f8a471cc93df4fdfc07bb
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-09 09:22:54 -0400

    Refactored admin_services_card.js

--- src/components/cards/admin_services_card.js
+++ src/components/cards/admin_services_card.js
@@ -2,22 +2,29 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 
 const AdminServicesCard = (props) => {
-
-    const clickListener = (id, str) => {
-        props.clickHandler(id, str)
-    }
+	const clickListener = (id, str) => {
+		props.clickHandler(id, str);
+	};
 	return (
-		<React.Fragment>
+		<>
 			<h5>{props.service.price}</h5>
 			<h6>{props.service.name}</h6>
 			<p>{props.service.description}</p>
-			<Button variant="danger" size="sm" onClick={ () => clickListener(props.service.id, "Delete") }>
+			<Button
+				variant="danger"
+				size="sm"
+				onClick={() => clickListener(props.service.id, "Delete")}
+			>
 				Delete Service
 			</Button>
-			<Button variant="warning" size="sm" onClick={ () => clickListener(props.service.id, "Edit") }>
+			<Button
+				variant="warning"
+				size="sm"
+				onClick={() => clickListener(props.service.id, "Edit")}
+			>
 				Edit Service
 			</Button>
-		</React.Fragment>
+		</>
 	);
 };
 
