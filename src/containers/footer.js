18b42d39fa2accdceafb3d948c6ab9887d592a61
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-08 20:41:57 -0400

    Refactored foot.js styles and converted from class to simple return function

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,34 +1,22 @@
 import React from "react";
+import "./css/containers.css";
 import Map from "../components/map";
 import GymContact from "../components/gym_contact";
-import styled from "styled-components";
 import Signup from "../components/forms/signup_form";
-import { Row, Col} from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
-const Styles = styled.div`
-	#gym-info {
-		background-color: #fcba03;
-	}
-`;
-
-class Footer extends React.Component {
-	render() {
-		return (
-			<Styles>
-				<div id="gym-info">
-					<Map />
-						<Row>
-							<Col>
-								<GymContact />
-							</Col>
-							<Col>
-								<Signup />
-							</Col>
-						</Row>
-				</div>
-			</Styles>
-		);
-	}
-}
+const Footer = () => (
+	<div className="gym-color">
+		<Map />
+		<Row>
+			<Col>
+				<GymContact />
+			</Col>
+			<Col>
+				<Signup />
+			</Col>
+		</Row>
+	</div>
+);
 
 export default Footer;
