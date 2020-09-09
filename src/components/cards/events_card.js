27bdfaa126185deb99b3e649fd37baceca97cc8a
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-09 10:38:08 -0400

    Added padding bottom to services_card.js

--- src/components/cards/events_card.js
+++ src/components/cards/events_card.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "../css/cards.css"
 import { Card, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 const EventCard = (props) => (
 	<Col>
 		<Card style={{ width: "50 rem", border: "0px solid rgba(0,0,0,.7)", marginTop: "30px", marginBottom: "30px", backgroundColor: "rgba(0,0,0,.5)" }}>
-			<Card.Title style={{ color: "#fcba03" }} >{props.event.name}</Card.Title>
+			<Card.Title className="gym-text" >{props.event.name}</Card.Title>
 			<Card.Img
 				variant="top"
 				src={props.event.picture}
 				alt="Wodapalooza event"
 			/>
-			<Card.Text style={{ color: "#fcba03" }} >{props.event.description}</Card.Text>
+			<Card.Text className="gym-text" >{props.event.description}</Card.Text>
 			<Button variant="link">
 				<a href={props.event.url}>More Information</a>
 			</Button>
