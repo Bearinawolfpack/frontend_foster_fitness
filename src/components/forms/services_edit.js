a6272ef059434936a2ff3dfef26f231a054278b8
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-09 11:07:43 -0400

    removed layout.js

--- src/components/forms/services_edit.js
+++ src/components/forms/services_edit.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Layout from "../layout";
 import { Container } from "react-bootstrap"
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
