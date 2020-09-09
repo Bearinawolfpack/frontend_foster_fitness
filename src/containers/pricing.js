f5046585b4bf8ecb478f70e8140453f5f91050a9
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-08 20:35:36 -0400

    Reformatted pricing.js styles

--- src/containers/pricing.js
+++ src/containers/pricing.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ServicesCard from "../components/cards/services_card";
 import { Row, Container } from "react-bootstrap";
-import styled from "styled-components";
-import bottom_rings_bw from "../assets/bottom_rings_bw.jpeg";
-
-const Wrapper = styled.div`
-	background-image: url(${bottom_rings_bw});
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	background-attachment: fixed;
-	height: 40rem;
-`;
 
 class Pricing extends React.Component {
 	state = { stateService: [] };
@@ -27,13 +16,13 @@ class Pricing extends React.Component {
 		));
 
 		return (
-			<Wrapper>
+			<div className="bg-format rings_bw img-adjust">
 				<Container>
 					<Row xs={1} md={2} lg={3}>
 						{gymServices}
 					</Row>
 				</Container>
-			</Wrapper>
+			</div>
 		);
 	}
 }
