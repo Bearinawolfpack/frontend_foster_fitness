754ac5e0059bb684ffdc5ef8e65316e556e5a7ff
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-08 21:14:55 -0400

    Refactored jumbotron.js styles and simplified function by changing the return from explicit to implicit.

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 
 const Jumbotron = () => (
-		<Jumbo fluid className="jumbo">
-			<div className="overlay"></div>
-			<Container id="center-jumbo-content">
-				<h1>Foster Fitness</h1>
-				<p>
-					This Place Shall Never Be With Those Cold And Timid Souls
-					Who Neither Know Victory Nor Defeat!
-				</p>			
-			</Container>
-		</Jumbo>
-	)
+	<Jumbo fluid className="jumbo">
+		<div className="overlay"></div>
+		<Container id="center-jumbo-content">
+			<h1>Foster Fitness</h1>
+			<p>
+				This Place Shall Never Be With Those Cold And Timid Souls Who
+				Neither Know Victory Nor Defeat!
+			</p>
+		</Container>
+	</Jumbo>
+);
 
 export default Jumbotron;
