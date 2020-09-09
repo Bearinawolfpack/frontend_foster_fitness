d038769313b5e3f9c2d90b1adb712c30e51ded44
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-09 10:19:58 -0400

    Partially refactored styles on coach_show.js

--- src/components/cards/coach_show.js
+++ src/components/cards/coach_show.js
@@ -1,16 +1,7 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import styled from "styled-components";
-import standing_bw_high from "../../assets/cf_ambience.jpeg";
-
-const Wrapper = styled.div`
-	background-image: url(${standing_bw_high});
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	background-attachment: fixed;
-	height: 100%;
-`;
+import "../css/cards.css";
+import { Row, Col } from "react-bootstrap";
+{/*adding className to col makes div with class col which changes the formatting*/}
 
 class ShowCoach extends React.Component {
 	state = { stateCoach: {} };
@@ -23,22 +14,35 @@ class ShowCoach extends React.Component {
 
 	render() {
 		return (
-			<Wrapper>
+			<div className="bg-format cf-ambience">
 				<Row>
-					<Col style={{ paddingBottom: "50px", marginTop: "50px",marginLeft: "50px"}}>
-					<img
-						src={this.state.stateCoach.picture}
-						alt="Foster Fitness Coach"
-						width="500"
-						height="500"
-					/>
+					<Col
+						style={{
+							paddingBottom: "50px",
+							marginTop: "50px",
+							marginLeft: "50px",
+						}}
+					>
+						<img
+							src={this.state.stateCoach.picture}
+							alt="Foster Fitness Coach"
+							width="500"
+							height="500"
+						/>
 					</Col>
-					<Col style={{ paddingTop: "50px", backgroundColor: "rgba(0,0,0,.5)" }} >
-						<h3 style={{ color: "#fcba03" }} >{this.state.stateCoach.name}</h3>
-						<p style={{ color: "#fcba03" }} >{this.state.stateCoach.bio}</p>
+					<Col
+						style={{
+							paddingTop: "50px",
+							backgroundColor: "rgba(0,0,0,.5)",
+						}}
+					>
+						<h3 className="gym-text">
+							{this.state.stateCoach.name}
+						</h3>
+						<p className="gym-text">{this.state.stateCoach.bio}</p>
 					</Col>
 				</Row>
-			</Wrapper>
+			</div>
 		);
 	}
 }
