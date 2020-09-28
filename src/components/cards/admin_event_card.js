3c5db24e9184935465c388d3117f0d8778d50aab
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-28 16:51:51 -0400

    Reformatted admin_event_card.js

--- src/components/cards/admin_event_card.js
+++ src/components/cards/admin_event_card.js
@@ -1,39 +1,39 @@
-import React from "react";
-import { Container } from 'react-bootstrap'
-import Button from "react-bootstrap/Button";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const AdminEventCard = (props) => {
-	const clickListener = (id, str) => {
-		props.clickHandler(id, str);
-	};
-	return (
-		<Container>
-            <br/>
-			<img
-				src={props.event.picture}
-				alt="Foster Fitness Event"
-				width="350"
-				height="250"
-			/>
-            <br/>
-			<h6>{props.event.name}</h6>
-			<p>{props.event.description}</p>
-			<Button
-				variant="danger"
-				size="sm"
-				onClick={() => clickListener(props.event.id, "Delete")}
-			>
-				Delete Event
-			</Button>
-			<Button
-				variant="warning"
-				size="sm"
-				onClick={() => clickListener(props.event.id, "Edit")}
-			>
-				Edit Event
-			</Button>
-		</Container>
-	);
+  const clickListener = (id, str) => {
+    props.clickHandler(id, str);
+  };
+  return (
+    <Container>
+      <br />
+      <img
+        src={props.event.picture}
+        alt="Foster Fitness Event"
+        width="350"
+        height="250"
+      />
+      <br />
+      <h6>{props.event.name}</h6>
+      <p>{props.event.description}</p>
+      <Button
+        variant="danger"
+        size="sm"
+        onClick={() => clickListener(props.event.id, 'Delete')}
+      >
+        Delete Event
+      </Button>
+      <Button
+        variant="warning"
+        size="sm"
+        onClick={() => clickListener(props.event.id, 'Edit')}
+      >
+        Edit Event
+      </Button>
+    </Container>
+  );
 };
 
 export default AdminEventCard;
