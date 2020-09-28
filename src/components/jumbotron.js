9cea9980eabca8769474d395218478738761efc5
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-28 16:13:41 -0400

    Made div self-closing in jumbotron.js

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -4,7 +4,7 @@ import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 
 const Jumbotron = () => (
   <Jumbo fluid className="jumbo-format cf-ambience">
-    <div className="overlay"></div>
+    <div className="overlay" />
     <Container id="center-jumbo-content">
       <h1 className="gym-text">Foster Fitness</h1>
       <p>
