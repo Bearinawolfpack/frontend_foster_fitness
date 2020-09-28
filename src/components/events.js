efdfe6f813e77c2e11f7b1ecccc2ade92629791d
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-28 16:03:26 -0400

    Reformatted events.js component.

--- src/components/events.js
+++ src/components/events.js
@@ -1,24 +1,21 @@
-import React from 'react'
-import EventCard from './cards/events_card'
+import React from 'react';
+import EventCard from './cards/events_card';
 
 class Events extends React.Component {
-    state = { stateEvents:[] }
+  state = { stateEvents: [] };
 
-    componentDidMount(){
-        fetch('http://localhost:3000/events')
-        .then(resp => resp.json())
-        .then(events => this.setState({stateEvents: events}))
-    }
+  componentDidMount() {
+    fetch('http://localhost:3000/events')
+      .then((resp) => resp.json())
+      .then((events) => this.setState({ stateEvents: events }));
+  }
 
-    render() {
-        let events = this.state.stateEvents.map(event => <EventCard event={event} key={event.id}/>)
-        return(
-            <React.Fragment>
-                {events}
-            </React.Fragment>
-
-        )
-    }
+  render() {
+    let events = this.state.stateEvents.map((event) => (
+      <EventCard event={event} key={event.id} />
+    ));
+    return <React.Fragment>{events}</React.Fragment>;
+  }
 }
 
-export default Events
+export default Events;
