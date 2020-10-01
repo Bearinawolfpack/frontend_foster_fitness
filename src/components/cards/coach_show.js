39e2ba6d6eec47c455c010a22aa4694735c106b7
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-01 15:44:13 -0400

    Reformat coach_show.js

--- src/components/cards/coach_show.js
+++ src/components/cards/coach_show.js
@@ -1,52 +1,47 @@
-import React from "react";
-import "../../App.css"
-import { Row, Col } from "react-bootstrap";
-{
-	/*adding className to col creates a div with className col which changes the formatting*/
-}
+import React from 'react';
+import '../../App.css';
+import { Row, Col } from 'react-bootstrap';
 
 class ShowCoach extends React.Component {
-	state = { stateCoach: {} };
+  state = { stateCoach: {} };
 
-	componentDidMount() {
-		fetch(`http://localhost:3000/coaches/${this.props.match.params.id}`)
-			.then((resp) => resp.json())
-			.then((coach) => this.setState({ stateCoach: coach }));
-	}
+  componentDidMount() {
+    fetch(`http://localhost:3000/coaches/${this.props.match.params.id}`)
+      .then((resp) => resp.json())
+      .then((coach) => this.setState({ stateCoach: coach }));
+  }
 
-	render() {
-		return (
-			<div className="bg-format cf-ambience">
-				<Row>
-					<Col
-						style={{
-							paddingBottom: "50px",
-							marginTop: "50px",
-							marginLeft: "50px",
-						}}
-					>
-						<img
-							src={this.state.stateCoach.picture}
-							alt="Foster Fitness Coach"
-							width="500"
-							height="500"
-						/>
-					</Col>
-					<Col
-						style={{
-							paddingTop: "50px",
-							backgroundColor: "rgba(0,0,0,.5)",
-						}}
-					>
-						<h3 className="gym-text">
-							{this.state.stateCoach.name}
-						</h3>
-						<p className="gym-text">{this.state.stateCoach.bio}</p>
-					</Col>
-				</Row>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="bg-format cf-ambience">
+        <Row>
+          <Col
+            style={{
+              paddingBottom: '50px',
+              marginTop: '50px',
+              marginLeft: '50px',
+            }}
+          >
+            <img
+              src={this.state.stateCoach.picture}
+              alt="Foster Fitness Coach"
+              width="500"
+              height="500"
+            />
+          </Col>
+          <Col
+            style={{
+              paddingTop: '50px',
+              backgroundColor: 'rgba(0,0,0,.5)',
+            }}
+          >
+            <h3 className="gym-text">{this.state.stateCoach.name}</h3>
+            <p className="gym-text">{this.state.stateCoach.bio}</p>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default ShowCoach;
