b48e7b1eed6e8638a5fa61834b03010c504b6195
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-02 15:18:11 -0400

    Refactor login_form.js

--- src/components/forms/login_form.js
+++ src/components/forms/login_form.js
@@ -1,85 +1,90 @@
-import React from "react";
-import { Container } from "react-bootstrap"
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { Redirect } from "react-router-dom"
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import { Redirect } from 'react-router-dom';
+
+const localhostAdmin = 'http://localhost:3000/admins';
 
 class LoginForm extends React.Component {
-	state = {
-		name: "",
-		password: "",
-        admin: [],
-        history: [],
-        loggedIn: false
-    };
-    
-	componentDidMount() {
-		fetch("http://localhost:3000/admins")
-			.then((resp) => resp.json())
-			.then((admin) => this.setState({ admin: admin }));
-	}
+  state = {
+    name: '',
+    password: '',
+    admin: [],
+    loggedIn: false,
+    alert: false,
+  };
 
-	changeHandler = (event) => {
-		this.setState({ [event.target.name]: event.target.value });
-	};
+  componentDidMount() {
+    fetch(`${localhostAdmin}`)
+      .then((resp) => resp.json())
+      .then((admin) => this.setState({ admin }));
+  }
 
-	login = (event) => {
-        event.preventDefault();
-		if (
-			this.state.admin[0].name === this.state.name &&
-			this.state.admin[0].password === this.state.password
-		) {
-            this.setState(
-				{ loggedIn: true }
-			)
-		} else {
-			alert("Please Try Again");
-			this.resetSignup();
-		}
-	};
+  changeHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-	resetSignup = () => {
-		this.setState({
-			name: "",
-			password: "",
-		});
-	};
+  login = (event) => {
+    event.preventDefault();
+    if (
+      this.state.admin[0].name === this.state.name &&
+      this.state.admin[0].password === this.state.password
+    ) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ alert: true });
+      this.resetSignup();
+    }
+  };
 
-	render() {
-        let loggedIn = this.state.loggedIn
-        if(loggedIn){
-            return <Redirect to="/admin"/>
-        }
+  resetSignup = () => {
+    this.setState({
+      name: '',
+      password: '',
+    });
+  };
 
-		return (
-			<Container>
-				<h1>Login</h1>
-				<Form onSubmit={this.login}>
-					<Form.Group>
-						<Form.Label>Name:</Form.Label>
-						<Form.Control
-							type="text"
-							name="name"
-							placeholder="Enter Your Name"
-							value={this.state.name}
-							onChange={this.changeHandler}
-						/>
-						<Form.Label>Password:</Form.Label>
-						<Form.Control
-							type="password"
-							name="password"
-							placeholder="Enter Your Password"
-							value={this.state.password}
-							onChange={this.changeHandler}
-						/>
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Submit
-					</Button>
-				</Form>
-			</Container>
-		);
-	}
+  render() {
+    const { loggedIn } = this.state;
+    const { alert } = this.state;
+    if (loggedIn) {
+      return <Redirect to="/admin" />;
+    }
+    if (alert) {
+      return <Alert variant="danger">Please Try Again</Alert>;
+    }
+
+    return (
+      <Container>
+        <h1>Login</h1>
+        <Form onSubmit={this.login}>
+          <Form.Group>
+            <Form.Label>Name:</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Enter Your Name"
+              value={this.state.name}
+              onChange={this.changeHandler}
+            />
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Enter Your Password"
+              value={this.state.password}
+              onChange={this.changeHandler}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default LoginForm;
