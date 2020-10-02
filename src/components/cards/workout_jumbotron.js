391a626d32140e1573d5e7f530e1af889ae8fb16
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-01 20:34:15 -0400

    Add propType validations to workout_jumbotron.js

--- src/components/cards/workout_jumbotron.js
+++ src/components/cards/workout_jumbotron.js
@@ -1,17 +1,27 @@
-import React from "react";
-import "../../App.css";
-import { Jumbotron as Jumbo, Container } from "react-bootstrap";
+import React from 'react';
+import '../../App.css';
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 const WorkoutJumbotron = (props) => (
-	<Jumbo fluid className="jumbo-format workout-jumbo bw-mess">
-		<div className="overlay"></div>
-		<Container id="center-jumbo-content">
-			<h3>Today is {props.wod.date} and the Workout for today is:</h3>
-			<h1 className="gym-text">{props.wod.name}</h1>
-			<h3>{props.wod.wod}</h3>
-			Summary: <h5>{props.wod.description}</h5>
-		</Container>
-	</Jumbo>
+  <Jumbo fluid className="jumbo-format workout-jumbo bw-mess">
+    <div className="overlay" />
+    <Container id="center-jumbo-content">
+      <h3>Today is {props.wod.date} and the Workout for today is:</h3>
+      <h1 className="gym-text">{props.wod.name}</h1>
+      <h3>{props.wod.wod}</h3>
+      Summary: <h5>{props.wod.description}</h5>
+    </Container>
+  </Jumbo>
 );
 
 export default WorkoutJumbotron;
+
+WorkoutJumbotron.propTypes = {
+  wod: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    wod: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
