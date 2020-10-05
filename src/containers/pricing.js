d7052519a92916aaffdf67a5da15f6c169632384
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-05 11:20:07 -0400

    Refactor pricing.js

--- src/containers/pricing.js
+++ src/containers/pricing.js
@@ -1,31 +1,35 @@
-import React from "react";
-import "../App.css"
-import ServicesCard from "../components/cards/services_card";
-import { Row, Container } from "react-bootstrap";
+import React from 'react';
+import '../App.css';
+import { Row, Container } from 'react-bootstrap';
+
+import ServicesCard from '../components/cards/services_card';
+
+const localhostServices = 'http://localhost:3000/services';
 
 class Pricing extends React.Component {
-	state = { stateService: [] };
+  state = { stateService: [] };
 
-	componentDidMount() {
-		fetch("http://localhost:3000/services")
-			.then((resp) => resp.json())
-			.then((services) => this.setState({ stateService: services }));
-	}
-	render() {
-		let gymServices = this.state.stateService.map((service) => (
-			<ServicesCard service={service} key={service.id} />
-		));
+  componentDidMount() {
+    fetch(`${localhostServices}`)
+      .then((resp) => resp.json())
+      .then((services) => this.setState({ stateService: services }));
+  }
 
-		return (
-			<div className="bg-format rings-bw img-adjust">
-				<Container>
-					<Row xs={1} md={2} lg={3}>
-						{gymServices}
-					</Row>
-				</Container>
-			</div>
-		);
-	}
+  render() {
+    const gymServices = this.state.stateService.map((service) => (
+      <ServicesCard service={service} key={service.id} />
+    ));
+
+    return (
+      <div className="bg-format rings-bw img-adjust">
+        <Container>
+          <Row xs={1} md={2} lg={3}>
+            {gymServices}
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Pricing;
