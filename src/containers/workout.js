61701a6a3425b517cf96c1bb0a3b4cba68b59928
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-05 11:22:31 -0400

    Refactor workout.js

--- src/containers/workout.js
+++ src/containers/workout.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import WorkoutJumbotron from '../components/cards/workout_jumbotron'
+import React from 'react';
 
-class Workout extends React.Component{
-    state = { stateWorkout: [] }
+import WorkoutJumbotron from '../components/cards/workout_jumbotron';
 
-    componentDidMount(){
-        fetch('http://localhost:3000/workouts')
-        .then(resp => resp.json())
-        .then(workout => this.setState({ stateWorkout: workout })) 
-    }
+const localhostWorkouts = 'http://localhost:3000/workouts';
 
-    render(){
-        let currentWorkout = this.state.stateWorkout.map(wod => <WorkoutJumbotron wod={wod} key={wod.id} />)
-        return(
-            <div>
-                {currentWorkout}
-            </div>
-        )
-    }
+class Workout extends React.Component {
+  state = { stateWorkout: [] };
+
+  componentDidMount() {
+    fetch(`${localhostWorkouts}`)
+      .then((resp) => resp.json())
+      .then((workout) => this.setState({ stateWorkout: workout }));
+  }
+
+  render() {
+    const currentWorkout = this.state.stateWorkout.map((wod) => (
+      <WorkoutJumbotron wod={wod} key={wod.id} />
+    ));
+    return <div>{currentWorkout}</div>;
+  }
 }
 
-
-export default Workout
+export default Workout;
